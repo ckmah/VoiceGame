0fc1e409517ebf373ad44509c61750aa72cd03ec
ckmah <clarence.iyan@gmail.com>
2014-09-05 17:43:49 -0700

    quick code/comments cleanup

--- Logic.m
+++ Logic.m
@@ -1,5 +1,6 @@
+% Logic function provides the game logic for a voice prosthetic game interface.
+% A player aims to capture targets scrolling across the screen by keyboard input/voice input.
 function f = Logic()
-  % f = 0;
 
   % STATE VARIABLES
   STATE_FAILURE = -1;
@@ -8,25 +9,19 @@ function f = Logic()
   STATE_ACQUIRED = 2;
 
   %% Define computer-specific variables
-  ipA = '137.110.63.45';  portA = 9052;   % Modify these values to be those of your first computer.
-  ipB = '169.228.172.179';  portB = 9051;  % Modify these values to be those of your second computer.
+  ipA = '137.110.62.104';  portA = 8052;   % Modify these values to be those of this computer.
+  ipB = '137.110.62.56';  portB = 8051;  % Modify these values to be those of the receiving computer.
 
   time = 0; % relative time (x-axis)
   DELAY = 0.1; % pause between iterations
 
-  % PLAYER
-  % myposx = 0; % track my position on x-axis (should always match time)
-  % myposy = 0; % track my position on y-axis (user controllable)
   p = 0; % player object
 
   % TARGET
-  %targetx = 0; % instantiate target xpos
-  %targety = 0; % instantiate target ypos
   NUMTARGETS = 10;
   targets(NUMTARGETS) = Target();
 
   % KEYBOARD KEY VALUES
-  % KbName('UnifyKeyNames');
   kc_return = KbName('Return');
   kc_up = KbName('up');
   kc_down = KbName('down');
@@ -44,10 +39,10 @@ function f = Logic()
         disp('Initializing new player...');
         p = Player(1,1,5,true); % generate Player object
 
-        % generate targets
-        % for n = 1:10
-        %    targets(n) = Target(n*50,10,2,true);
-        % end
+        generate targets
+        for n = 1:10
+           targets(n) = Target(n*50,10,2,true);
+        end
 
         udpA = udp(ipB,portB,'LocalPort',portA); % Create UDP Object
         fopen(udpA); % Connect to UDP Object
@@ -78,9 +73,9 @@ function f = Logic()
       case STATE_FAILURE
         fprintf('Failed to acquire target.');
         break;
+
       otherwise
-        % Do additional computations, possibly including more calls to KbQueueCheck, etc
-        KbQueueRelease;
+        KbQueueRelease; % clear keyboard queue
         f = error('Invalid state.');
         return;
     end
@@ -90,40 +85,38 @@ function f = Logic()
     if keyIsDown
       pressedCodes = find(keycodes);
       for i = 1:size(pressedCodes,2)
-        % fprintf('The %s key was pressed at time %.3f seconds\n', KbName(pressedCodes(i)), keycodes(pressedCodes(i))-startTime);
-        % fprintf(udpA, KbName(pressedCodes(i)));
-
         % format player data output
         pdata = strcat('player,', num2str(p.x), ',', num2str(p.y), ',', num2str(p.radius), ',', num2str(p.visible), ';');
         tdata = '';
 
         % format target data output
         for n = 1:10
-          % tdata = strcat(tdata, 'target,', num2str(targets(n).x), ',', num2str(targets(n).y), ',', num2str(targets(n).radius), ',', num2str(targets(n).visible), ';');
+          tdata = strcat(tdata, 'target,', num2str(targets(n).x), ',', num2str(targets(n).y), ',', num2str(targets(n).radius), ',', num2str(targets(n).visible), ';');
         end
 
-        % data = strcat(pdata, tdata);
-        data = strcat(num2str(10),',',num2str(1));
+        data = strcat(pdata, tdata);
         disp(pdata); % debug print
         fprintf(udpA, pdata); % UDP output
 
+        fprintf(udpA, strcat(num2str(TEMP), '0000'));
+
         % exit on return
         if pressedCodes(i) == kc_return
-          %% Clean Up Machine A
+          %% clean up
           fclose(udpA);
           delete(udpA)
           clear ipA portA ipB portB udpA
           return;
+
         % move player up y-axis one unit
         elseif pressedCodes(i) == kc_up
           p.y = p.y + 1;
+
         % move player up y-axis one unit
         elseif pressedCodes(i) == kc_down
           p.y = p.y - 1;
         end
       end
-    else
-      % fprintf('No key presses detected\n');
     end
 
     time = time + 1; % increment
